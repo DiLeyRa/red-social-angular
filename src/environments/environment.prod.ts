a0847a674ff37a49abfd3e36bd0860ee55f032b0
DiLeyRa <ing.dianaleyva16@gmail.com>
2018-09-21 14:16:20 -0500

    Modificando la maquetacion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig:{
+    apiKey: "AIzaSyC3ZQZRHElsHDKd2OEUcxwlbV7DnG54O2E",
+    authDomain: "redsocialangular-c7d10.firebaseapp.com",
+    databaseURL: "https://redsocialangular-c7d10.firebaseio.com",
+    projectId: "redsocialangular-c7d10",
+    storageBucket: "redsocialangular-c7d10.appspot.com",
+    messagingSenderId: "775855254332"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig:{
+    apiKey: "AIzaSyC3ZQZRHElsHDKd2OEUcxwlbV7DnG54O2E",
+    authDomain: "redsocialangular-c7d10.firebaseapp.com",
+    databaseURL: "https://redsocialangular-c7d10.firebaseio.com",
+    projectId: "redsocialangular-c7d10",
+    storageBucket: "redsocialangular-c7d10.appspot.com",
+    messagingSenderId: "775855254332"
+  }
 };
 
 /*
